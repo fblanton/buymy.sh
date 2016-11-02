16ff310252e1b15bde929127a95ca3612c38d3ce
Forrest Blanton <fblanton@gmail.com>
2016-11-01 20:20:11 -0700

    User can view item details. #17

--- client/shop/shop.controller.js
+++ client/shop/shop.controller.js
@@ -2,9 +2,11 @@ angular
   .module('app')
   .controller('Shop', Shop)
 
-function Shop($scope, shop, items) {
+function Shop($scope, shop, items, ngMeta) {
   const vm = this
   vm.items = items
 
   Object.assign(vm, shop)
+  ngMeta.setTitle(shop.title + ' | ' + shop.description)
+  ngMeta.setTag('description', shop.description)
 }
